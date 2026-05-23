eb5ffb768f5df1ccdf09e63f90a6a8a79b18af70
Sravan Kumar <nagbishal07@gmail.com>
2026-05-23 23:37:05 +0530

    fixed emp id

--- public/script.js
+++ public/script.js
@@ -3,45 +3,26 @@ document.getElementById("studentForm")
 
     e.preventDefault();
 
-    const phone = document.getElementById("whatsapp").value;
-
-    if(phone.length !== 10) {
-        alert("Phone number must be exactly 10 digits");
-        return;
-    }
-
     const data = {
-
         firstName: document.getElementById("firstName").value,
-
         lastName: document.getElementById("lastName").value,
-
         domain: document.getElementById("domain").value,
-
-        whatsapp: phone,
-
+        whatsapp: document.getElementById("whatsapp").value,
         email: document.getElementById("email").value,
-
-        tenure: document.getElementById("tenure").value + " Months",
-
+        tenure: document.getElementById("tenure").value,
         joiningDate: document.getElementById("joiningDate").value
     };
 
     const response = await fetch("/register", {
-
         method: "POST",
-
         headers: {
             "Content-Type": "application/json"
         },
-
         body: JSON.stringify(data)
-
     });
 
     const result = await response.json();
 
     document.getElementById("result").innerHTML =
         "Generated Employee ID: " + result.employeeId;
-
 });
